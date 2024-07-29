8a5a41a875c9bf00a019da0a3662531c7fbc3e8f
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-07-29 20:06:32 +0300

    koleksiyon yapılarında sabit liste gösterildi.

--- koleksiyon_yapıları/sabit_liste.dart
+++ koleksiyon_yapıları/sabit_liste.dart
@@ -2,4 +2,9 @@ void main(List<String> args) {
   List<int> sayilar  = List.filled(10,0);
   sayilar[0] = 10;
   print(sayilar);
+
+  for (int i=0; i<sayilar.length; i ++){
+    print(sayilar[i]+ 5);
+    
+  }
 }
